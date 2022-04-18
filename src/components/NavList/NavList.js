09e75b32b60ffd8cb4c7bff9ef89864fa380c4c4
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-04-18 21:04:39 +0500

    add component NavList

---
+++ src/components/NavList/NavList.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './NavList.css';
+
+const folders = [
+  {
+    id: 1,
+    name: 'Входящие',
+  },
+  {
+    id: 2,
+    name: 'Отправленные',
+  },
+  {
+    id: 3,
+    name: 'Черновики',
+  },
+  {
+    id: 4,
+    name: 'Спам',
+  },
+  {
+    id: 5,
+    name: 'Корзина',
+  },
+];
+
+function NavList({ currentFolder, setCurrentFolder }) {
+  function handleFolderClick(id) {
+    setCurrentFolder(id);
+  }
+
+  return (
+    <ul className="nav__list">
+      {folders.map(({ name, id }) => (
+        <li className={`nav__folders ${currentFolder === id ? 'nav__folders_active' : ''}`} onClick={() => handleFolderClick(id)} key={id}>{name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default NavList;
